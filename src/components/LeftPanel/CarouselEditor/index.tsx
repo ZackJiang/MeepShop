b7b42308c15d5179bb5d112068fe11f1e07e9a2b
ZackJiangNoodoe <zackjiang@noodoe.com>
2024-04-10 14:04:29 +0800

    Allow to append and delete url

--- src/components/LeftPanel/CarouselEditor/index.tsx
+++ src/components/LeftPanel/CarouselEditor/index.tsx
@@ -44,24 +44,55 @@ export default function CarouselEditor(props: ICarouselEditorProps) {
         }}
       />
       {element.urls.map((url, index) => (
-        <input
-          key={index}
-          type="text"
-          className="w-full text-black mt-4"
-          defaultValue={url}
-          onChange={(event) => {
-            if (isValidUrl(event.target.value)) {
+        <div key={index}>
+          <input
+            type="text"
+            className="w-full text-black mt-4"
+            defaultValue={url}
+            onChange={(event) => {
+              if (isValidUrl(event.target.value)) {
+                let newUrls = [...element.urls];
+                newUrls[index] = event.target.value;
+                dispatch(
+                  updateElement({
+                    ...element,
+                    urls: newUrls,
+                  })
+                );
+              }
+            }}
+          />
+          <button
+            className="bg-orange-900"
+            onClick={() => {
               let newUrls = [...element.urls];
-              newUrls[index] = event.target.value;
+              newUrls.splice(index, 0, newUrls[index]);
               dispatch(
                 updateElement({
                   ...element,
                   urls: newUrls,
                 })
               );
-            }
-          }}
-        />
+            }}
+          >
+            append
+          </button>
+          <button
+            className="bg-orange-900 ml-2"
+            onClick={() => {
+              let newUrls = [...element.urls];
+              newUrls.splice(index, 1);
+              dispatch(
+                updateElement({
+                  ...element,
+                  urls: newUrls,
+                })
+              );
+            }}
+          >
+            delete
+          </button>
+        </div>
       ))}
     </>
   );
--- src/components/LeftPanel/TextEditor/index.tsx
+++ src/components/LeftPanel/TextEditor/index.tsx
@@ -1,5 +1,5 @@
 import dynamic from "next/dynamic";
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import { ITextElement } from "@/store/elements";
 import { updateElement } from "@/store/elements";
 import { useAppDispatch } from "@/store/store";
